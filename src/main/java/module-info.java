b09626264a09eb215462954fa91d7050276ae556
ajfait <jafait1004@gmail.com>
2025-04-17 19:21:37 -0500

    troubleshoot and resolve version conflicts

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.openjfx.javafxmavenarchetypes {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens org.openjfx.javafxmavenarchetypes to javafx.fxml;
+    exports org.openjfx.javafxmavenarchetypes;
+}
